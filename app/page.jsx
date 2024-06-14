53c760443a7ee13798ba92289a570b65a419ff4a
Anica1004 <anicamok@gmail.com>
2024-06-13 18:51:20 -0700

    File Change: Moved navigation bar to the layout pg

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import NavBar from "./components/NavigationBar/NavBar";
-
 export default function Home() {
   return (
     <main className="min-h-screen"> 
-       <NavBar/>   
+      <div>
+        
+      </div>
     </main>
   );
 }
