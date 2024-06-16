f13700e5719cb50fdfaf3741d6feb17b2cb943a5
cathylei <zlei04@students.cs.ubc.ca>
2024-06-15 21:11:16 -0700

    corrected env file, switched from nlpcloud to huggingface api

--- app/EmotionAnalyzer.jsx
+++ app/EmotionAnalyzer.jsx
@@ -1,32 +1,62 @@
-const toneApiKey = process.env.TONE_API_KEY;
+"use client";
+import React, { useState } from "react";
+import "./EmotionAnalyzer.css";
 
-const NLPCloudClient = require("nlpcloud");
-const model = "distilbert-base-uncased-emotion";
+function EmotionAnalyzer() {
+  const TONE_API_KEY = process.env.NEXT_PUBLIC_TONE_API_KEY;
+  const [analysis, setAnalysis] = useState("");
+  const [text, setText] = useState("");
 
-const EMOTIONS = {
-  JOY: "joy",
-  SURPRISE: "surprise",
-  FEAR: "fear",
-  ANGER: "anger",
-  SADNESS: "sadness",
-  LOVE: "love",
-};
+  const EMOTIONS = {
+    JOY: "joy",
+    SURPRISE: "surprise",
+    FEAR: "fear",
+    ANGER: "anger",
+    SADNESS: "sadness",
+    LOVE: "love",
+  };
 
-var emotion = "";
+  async function query(data) {
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/finiteautomata/bertweet-base-emotion-analysis",
+      {
+        headers: {
+          Authorization: `Bearer ${TONE_API_KEY}`,
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    return result;
+  }
 
-const client = new NLPCloudClient({ model: model, token: toneApiKey });
-// Returns an Axios promise with the results.
-// In case of success, results are contained in `response.data`.
-// In case of failure, you can retrieve the status code in `err.response.status`
-// and the error message in `err.response.data.detail`.
-client
-  .sentiment({ text: "NLP Cloud proposes an amazing service!" })
-  .then(function (response) {
-    console.log(response.data);
-    emotion = response.data.scored_labels[0].label;
-    console.log(emotion);
-  })
-  .catch(function (err) {
-    console.error(err.response.status);
-    console.error(err.response.data.detail);
-  });
+  function analyzeText(e) {
+    e.preventDefault();
+    query({ inputs: text }).then((response) => {
+      console.log(JSON.stringify(response));
+      setAnalysis(JSON.stringify(response));
+    });
+  }
+
+  return (
+    <form className="analyzer" onSubmit={analyzeText}>
+      <label> Emotion Analyzer </label>
+      <br></br>
+      <textarea
+        className="text-box"
+        rows={6}
+        cols={50}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <br></br>
+      <button className="submit-button" type="submit">
+        Analyze
+      </button>
+      <h1> Detected emotion: {analysis} </h1>
+    </form>
+  );
+}
+
+export default EmotionAnalyzer;
